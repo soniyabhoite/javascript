4028e84dbfdada0fc3e5f345fc86136192fc5d5e
soniyabhoite <soniyabhoite223@gmail.com>
2023-10-27 09:29:45 +0530

    datatypes

---
+++ Day1/Datatypes.js
@@ -0,0 +1,9 @@
+console.log("string Datatype-")
+console.log("Hello ");
+console.log('awdiz');
+console.log(`students`);
+console.log("1"+'2')
+console.log('1'+3)
+console.log("Number Datatype-")
+console.log(1);
+console.log(1+2)
