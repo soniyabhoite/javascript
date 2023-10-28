bf46fa58922f302079def011834293290879b846
soniyabhoite <soniyabhoite223@gmail.com>
2023-10-28 15:15:41 +0530

    operators

---
+++ Day3/operators.js
@@ -0,0 +1,49 @@
+//awdiz790
+console.log("Boolean datatypes")
+console.log(true)
+console.log(false)
+console.log(typeof(true))
+console.log(typeof(flase))
+
+console.log("Arithmetic operator")
+console.log(20+63)
+console.log("string"+"addition")
+console.log(120-96)
+console.log("string"-"awdiz")
+console.log(85*30)
+console.log(50/2)
+console.log(50%2)
+
+console.log("logical operator")
+
+console.log(25<2)
+console.log(400>2000)
+console.log(45<=46)
+console.log(45<=45)
+
+var age=18
+console.log(age>=18 && age<91)
+console.log(age>18 || age<25)
+
+
+console.log("== and === Comparison Operator  ")
+var num1=45;
+var uname="awdiz"
+var num2=55
+console.log(num1==num2)
+console.log(num1===45)
+console.log(uname=="awdiz")
+console.log("12"===12)
+
+
+console.log("function ")
+function add(num1,num2)
+{
+    return num1+num2;
+}
+console.log(add(2,3))
+
+
+
+
+
