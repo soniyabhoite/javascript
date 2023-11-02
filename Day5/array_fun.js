77be90dcc9231719c89cc0157f3f71243a6c3907
soniyabhoite <soniyabhoite223@gmail.com>
2023-11-02 14:08:01 +0530

    for loop and array function

---
+++ Day5/array_fun.js
@@ -0,0 +1,26 @@
+var numbers=[10,20,30,40,50]
+console.log("array elements")
+console.log(numbers)
+console.log(numbers[0])
+
+//push and pop
+//push-> add ele in last position of array
+//pop -> remove the ele from last position of array
+console.log("push")
+numbers.push(60)
+numbers.push(70)
+console.log(numbers)
+console.log("pop")
+numbers.pop()
+console.log(numbers)
+
+//shift and unshift 
+//shift -> remove the ele from first postion of array
+//unshift -> add ele at first position of array
+
+numbers.shift()
+console.log("shift")
+console.log(numbers)
+console.log("unshift")
+numbers.unshift(100,200)
+console.log(numbers)
---
+++ Day5/loop.js
@@ -0,0 +1,14 @@
+for(var i=0;i<10;i++)
+{
+    console.log(i)
+}
+console.log("---------------------")
+for(var i=10; i>=0;i--)
+{
+   console.log(i)
+}
+console.log("---------------------")
+for(var i=1;i<=50;i+=10)
+{
+    console.log(i)
+}
