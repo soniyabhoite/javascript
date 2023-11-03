f39d101ef3cecd404c484a3c9d3d7024ab12f30e
soniyabhoite <soniyabhoite223@gmail.com>
2023-11-03 11:10:18 +0530

    updates

---
+++ Day6/loop.js
@@ -0,0 +1,23 @@
+//reverse Number
+for(var i=10;i>=0;i--)
+{
+    console.log(i)
+}
+
+//750-300 
+for(var i=750;i>=300;i-=100)
+{
+    console.log(i)
+}
+
+var students=["aaa","bbb","ccc","ddd"]
+for(var i=0;i<=students.length-1;i++)
+{
+    console.log(students[i])
+}
+
+console.log("reverse order")
+for(var i=students.length-1;i>=0;i--)
+{
+    console.log(students[i])
+}
