bbf2cf17dca31f6846ed3b17684b90b1a98ed3a0
soniyabhoite <soniyabhoite223@gmail.com>
2023-11-23 10:50:46 +0530

    update

---
+++ Day13/datatypes.js
@@ -0,0 +1,28 @@
+var myage= a => console.log("my age = ",a)
+
+myage(25)
+
+var uname="abc"
+console.log(uname)
+var uname="xyz"
+console.log(uname)
+address="xxx"
+console.log(address)    //hosting-->move delclaration on top of the file
+var address
+
+const city="mumbai"
+//const city="delhi"   // redeclaration and reassigning not allowed
+
+console.log(city)
+console.log(state)
+const state="MH"
+
+let age=12
+//let age=14    // redeclaration not allowed 
+console.log(age)
+console.log(pin)
+let pin=12
+
+
+
+
